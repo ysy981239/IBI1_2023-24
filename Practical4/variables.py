9e7fe3441c8a822f5bcbfb5b27c739ff37b4fa33
ysy981239 <161603904+ysy981239@users.noreply.github.com>
2024-03-06 13:38:21 +0800

    edit variables.py

--- Practical4/variables.py
+++ Practical4/variables.py
@@ -1,10 +1,9 @@
->>> a=40
->>> b=36
->>> c=30
->>> d=a-c
->>> e=b-c
->>> d
-10
->>> e
-6
->>>d is better
+a=40
+b=36
+c=30
+d=a-c
+e=b-c
+if d>e:
+    print("a is better")
+else:
+    print("b is better")
