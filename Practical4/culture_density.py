463b193e9433224f40b4c1f2c9ade6d8c9aacfb3
ysy981239 <161603904+ysy981239@users.noreply.github.com>
2024-03-24 22:54:08 +0800

    culture.py

--- Practical4/culture_density.py
+++ Practical4/culture_density.py
@@ -1,6 +1,10 @@
-x=5
-a=0
-if x<90:
-   a=a+1
-   x=2*x
-else  
+holiday = 0
+density = 5
+# cell line doubles
+while density <= 90:
+    density *= 2
+    # if culture density is over 90%, the holiday can't going on.
+    if density > 90: 
+        break
+    holiday += 1
+print ("I can stay away from the lab for" , holiday , "days.")
